2c74907c0799f2499bc80447313a94aff7a2686a
Turmio <turo.soisenniemi@outlook.com>
2017-03-06 23:47:25 +0200

    Added verify steps and trying to fix travis configuration

--- test/hg/bash/test-incoming.sh
+++ test/hg/bash/test-incoming.sh
@@ -3,6 +3,7 @@
 checkReturnCode() {
     if [ $1 -eq 1 ]; then
         eval $2
+        echo "FAILURE"
         exit 1
     fi
 }
@@ -11,16 +12,17 @@ REPO1=repository1
 REPO2=repository2
 SCRIPT_PATH=$(dirname `which $0`)
 ABSOLUTE=$(cd $SCRIPT_PATH; pwd)
-CLEANUP="source $SCRIPT_PATH/clean-repos.sh $REPO1 $REPO2"
+NEWBRANCH=$ABSOLUTE/newbranch
+EXISTING=$ABSOLUTE/existing
+CLOSED=$ABSOLUTE/closed
+RESULT=0
+CLEANUP="source $SCRIPT_PATH/clean-repos.sh $REPO1 $REPO2; rm $NEWBRANCH;  rm $EXISTING;  rm $CLOSED"
 
 source $ABSOLUTE/init-repos.sh $REPO1 $REPO2
 ret_code=$?
 checkReturnCode $ret_code $CLEANUP
 
 WRITE_RESULTS=$ABSOLUTE/write-file.sh
-NEWBRANCH=$ABSOLUTE/newbranch
-EXISTING=$ABSOLUTE/existing
-CLOSED=$ABSOLUTE/closed
 
 NP="$WRITE_RESULTS $NEWBRANCH"
 NE="$WRITE_RESULTS $EXISTING"
@@ -42,4 +44,51 @@ source $SCRIPT_PATH/close-branch.sh $ABSOLUTE/$REPO2
 ret_code=$? 
 checkReturnCode $ret_code $CLEANUP
 
+EXPECTED_NEW=("default")
+EXPECTED_EXISTING=("default")
+EXPECTED_CLOSED=("default")
+I=0
+while IFS='' read -r line || [[ -n "$line" ]]; do
+    linearr=($line)
+    if [ "${linearr[0]}" != "${EXPECTED_NEW[$I]}" ]; then
+        RESULT=1
+    fi
+    let "I++"
+done < "$NEWBRANCH"
+if [! $i -eq ${EXPECTED_NEW[@]} ]; then
+    RESULT=1
+fi
+I=0
+while IFS='' read -r line || [[ -n "$line" ]]; do
+    linearr=($line)
+
+    if [ "${linearr[0]}"  != "${EXPECTED_EXISTING[$I]}" ]; then
+        RESULT=1
+    fi
+    let "I++"
+done < "$EXISTING"
+
+if [! $i -eq ${EXPECTED_EXISTING[@]} ]; then
+    RESULT=1
+fi
+
+I=0
+while IFS='' read -r line || [[ -n "$line" ]]; do
+    linearr=($line)
+
+    if [ "${linearr[0]}" != "${EXPECTED_CLOSED[$I]}" ]; then
+        RESULT=1
+    fi
+    let "I++"
+done < "$CLOSED"
+
+if [! $i -eq ${EXPECTED_CLOSED[@]} ]; then
+    RESULT=1
+fi
+
 eval $CLEANUP
+if [ $RESULT -eq 1 ]; then
+    echo "FAILURE"
+else
+    echo "SUCCESS"
+fi
